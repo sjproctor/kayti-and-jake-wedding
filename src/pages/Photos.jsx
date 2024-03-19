9cdc85c2b8e6228ae1356d60d644427e547960fe
Sarah Proctor <sproctor950@gmail.com>
2024-03-19 13:01:53 -0700

    :lipstick: added two images to the photos section

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,6 +1,8 @@
 import AnchorLink from "react-anchor-link-smooth-scroll"
-import goth from "../assets/goth.jpeg"
 import bolo from "../assets/bolo.jpeg"
+import camping from "../assets/camping.jpeg"
+import goth from "../assets/goth.jpeg"
+import tableRock from "../assets/table-rock.jpeg"
 
 const Photos = () => {
   return (
@@ -17,6 +19,18 @@ const Photos = () => {
         <img
           src={bolo}
           alt="Jake in a bolo tie and Kayti in a green dress"
+          className="dark:shadow-black/30 m-5 mx-auto w-64 rounded-full shadow-lg"
+        />
+      </div>
+      <div className="lg:flex">
+        <img
+          src={camping}
+          alt="Kayti and Jake wearing overalls in the car on the way to a camping trip"
+          className="dark:shadow-black/30 m-5 mx-auto w-64 rounded-full shadow-lg"
+        />
+        <img
+          src={tableRock}
+          alt="Kayti and Jake with Lola on the top of Table Rock in Boise"
           className="dark:shadow-black/30 m-5 mx-auto w-64 rounded-full shadow-lg"
         />
       </div>
