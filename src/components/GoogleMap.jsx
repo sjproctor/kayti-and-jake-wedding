21ac24ef8e7b5357939a11fbe8f2a6a712381348
Sarah Proctor <sproctor950@gmail.com>
2024-03-21 14:23:56 -0700

    :recycle: moves width property into tailwind styles

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -2,7 +2,6 @@ const GoogleMap = () => {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2888.656877202617!2d-116.20506237345882!3d43.61368540498516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54aef8fb2f9e4577%3A0x51618f9ed9c28459!2sThe%20Basque%20Center!5e0!3m2!1sen!2sus!4v1711048099024!5m2!1sen!2sus"
-      width="600"
       height="450"
       className="h-80 w-screen"
       loading="lazy"
