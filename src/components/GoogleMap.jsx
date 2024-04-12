6517cb117a7700a964f5d307d5ae742b585e34a1
Sarah Proctor <sproctor950@gmail.com>
2024-04-12 16:18:58 -0700

    :bento: updates map to new party location

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,7 +1,7 @@
 const GoogleMap = () => {
   return (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2888.656877202617!2d-116.20506237345882!3d43.61368540498516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54aef8fb2f9e4577%3A0x51618f9ed9c28459!2sThe%20Basque%20Center!5e0!3m2!1sen!2sus!4v1711048099024!5m2!1sen!2sus"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.3743583453224!2d-116.23067982382713!3d43.577918171105715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54ae57e771e1c4d9%3A0x4afa883c20147fbb!2s2707%20Malibu%20Rd%2C%20Boise%2C%20ID%2083705!5e0!3m2!1sen!2sus!4v1712522979062!5m2!1sen!2sus"
       height="450"
       className="h-80 w-screen"
       loading="lazy"
