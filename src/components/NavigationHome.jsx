ff88c12f2870dc0416ddb442be7686e5d83eddc4
Sarah Proctor <sproctor950@gmail.com>
2024-04-17 18:03:56 -0700

    :lipstick: reworks logo in navbar for scroll navigation

--- src/components/NavigationHome.jsx
+++ src/components/NavigationHome.jsx
@@ -1,11 +1,11 @@
-import HeaderLogo from "./HeaderLogo"
+import NavigationLogo from "./NavigationScrollLogo"
 import { Link } from "react-router-dom"
 import { Navbar } from "flowbite-react"
 
 const NavigationHome = () => {
   return (
     <Navbar fluid rounded>
-      <HeaderLogo />
+      <NavigationLogo />
       <Link
         to="/"
         className="border-gray-100 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent block py-2 pl-3 pr-4 hover:cursor-pointer md:border-0 md:p-0"
