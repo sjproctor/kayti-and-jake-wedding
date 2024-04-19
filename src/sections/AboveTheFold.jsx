f237be075e1186bebb92e202e295b902567992f1
Sarah Proctor <sproctor950@gmail.com>
2024-04-19 14:19:21 -0700

    :recycle: refactored code structure for web and mobile view

--- src/sections/AboveTheFold.jsx
+++ src/sections/AboveTheFold.jsx
@@ -4,6 +4,17 @@ import heroMobile from "../assets/hero-mobile.jpeg"
 const AboveTheFold = () => {
   return (
     <>
+      {/* full screen view */}
+      <div className="hidden text-center md:contents">
+        <img
+          src={hero}
+          alt="Kayti and Jake sitting outdoors in the foreground of a mountain with wispy clouds in the sky"
+          className="opacity-50"
+        />
+        <div className="absolute left-0 right-0 top-12">
+          <h1 className="font-moontime md:text-9xl">Kayti and Jake</h1>
+        </div>
+      </div>
       {/* mobile view */}
       <div className="text-center md:hidden">
         <img
@@ -12,18 +23,7 @@ const AboveTheFold = () => {
           className="opacity-50"
         />
         <div className="absolute left-0 right-0 top-20">
-          <h1 className="font-moontime text-6xl">Kayti and Jake</h1>
-        </div>
-      </div>
-      {/* full screen view */}
-      <div className="hidden text-center md:contents">
-        <img
-          src={hero}
-          alt="Kayti and Jake sitting outdoors in the foreground of a mountain with wispy clouds in the sky"
-          className="object-cover opacity-50"
-        />
-        <div className="absolute left-0 right-0 top-20">
-          <h1 className="font-moontime text-9xl">Kayti and Jake</h1>
+          <h1 className="font-moontime text-7xl sm:text-9xl">Kayti and Jake</h1>
         </div>
       </div>
     </>
