8b63eefa41afdbbf5ba0829d05da529d7e29ded1
Sarah Proctor <sproctor950@gmail.com>
2024-04-19 14:16:09 -0700

    :lipstick: added montserrat font for text blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,14 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        moontime: ["Moontime", "sans-serif"],
-        alice: ["Alice", "sans-serif"]
+        moontime: ["Moontime", "serif"],
+        alice: ["Alice", "serif"],
+        garamond: ["Garamond", "serif"],
+        montserrat: ["Montserrat", "sans-serif"]
       },
       screens: {
         sm: "640px",
-        md: "1000px",
+        md: "950px",
         lg: "1024px",
         xl: "1280px",
         "2xl": "1536px"
@@ -41,6 +43,22 @@ export default {
     opacity: {
       50: ".50",
       75: ".75"
+    },
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3.5rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+      "8xl": "6rem",
+      "9xl": "10rem"
     }
   },
   plugins: []
