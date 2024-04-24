b8d96c699d485964d0e6a50c96c70e65f187cd3a
Sarah Proctor <sproctor950@gmail.com>
2024-04-23 19:50:31 -0700

    :sparkles: rework of party details section

--- src/sections/PartyTime.jsx
+++ src/sections/PartyTime.jsx
@@ -1,31 +1,98 @@
 import React, { Suspense } from "react"
-import NavigationButton from "../components/NavigationButton"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import Timeline from "../components/Timeline"
+import dlWedding from "../assets/dl-wedding.jpeg"
 const GoogleMap = React.lazy(() => import("../components/GoogleMap"))
-import { Link } from "react-router-dom"
 
 const PartyTime = () => {
   return (
-    <>
-      <div className="m-4" id="party">
-        <Link to="/" className="hover:cursor-pointer">
-          <h2 className="m-3 text-center font-moontime text-6xl">
+    <div id="party">
+      <div className="relative text-center">
+        <img
+          src={dlWedding}
+          alt="Kayti and Jake looking snazzy at Dan and Laura's wedding"
+          className="opacity-50"
+        />
+        <div className="absolute left-0 right-0 top-4 md:top-12">
+          <h2 className="font-moontime text-5xl md:text-9xl">
             Party Information
           </h2>
-        </Link>
-        <div className="mb-3 md:mx-10 lg:mx-40 lg:max-w-5xl">
-          You are invited to a gathering of friends and family to celebrate with
-          Kayti and Jake on August 31, 2024. The party will be in Kayti and
-          Jake&apos;s backyard.
         </div>
-        <div className="mb-3 md:mx-10 lg:mx-40 lg:max-w-5xl">
-          <p>Location: Kayti and Jake&apos;s House</p>
-          <p>Address: 2707 Malibu Road, Boise, Idaho 83702</p>
-          <p>Date: Saturday, August 31, 2024</p>
-          <p>Time: 6 - 9 pm</p>
-          <p>
-            Dress code: The average August 31 temperature in Boise is 86
-            degrees. Please wear whatever makes you feel most comfortable.
-          </p>
+      </div>
+      <p className="m-2 text-center font-alice text-xl uppercase text-gray md:text-2xl">
+        Celebration Schedule
+      </p>
+      <Timeline />
+      <p className="m-2 text-center font-alice text-xl uppercase text-gray md:text-2xl">
+        Details, Details, Details
+      </p>
+      <div className="mx-auto w-11/12 pt-4 text-center md:w-2/3">
+        <h3 className="text-lg font-extrabold">What to know before...</h3>
+        <p className="py-2">
+          <b>RSVP: </b>Please
+          <AnchorLink href="#rsvp" className="px-1">
+            submit the RSVP
+          </AnchorLink>
+          form by July 1, 2024. (Happy birthday, Kayti!) If your RSVP status
+          changes, please resubmit the form and add a note.
+        </p>
+        <p className="py-2">
+          <b>Attire: </b>We&apos;re gonna be outside on the grass and it&apos;s
+          gonna be hot, y&apos;all. Temperatures in August in Boise regularly
+          hit 90 degrees. Please wear what will make you feel comfortable. As a
+          guide, think: sundresses, flipflops, rompers, linen button-ups. Not so
+          much: swimwear or suits.
+        </p>
+        <p className="py-2">
+          <b>What to bring: </b>We will have everything needed for a fun party.
+          Additional beverage contributions are very appreciated.
+        </p>
+        <br />
+        <h3 className="text-lg font-extrabold">
+          What to know as you arrive...
+        </h3>
+        <p className="py-2">
+          <b>Timing: </b>Need to come late or leave early? No problem. We know
+          life is busy so do your thing.
+        </p>
+        <p className="py-2">
+          <b>Parking: </b>All parking is on the street in Kayti and Jake&apos;s
+          neighborhood. Spots close to the house will fill up early so be
+          prepared to walk a few blocks. Please be respectful of Kayti and
+          Jake&apos;s neighbors! We HIGHLY recommend using ride shares.
+        </p>
+        <br />
+        <h3 className="text-lg font-extrabold">What to know during...</h3>
+        <p className="py-2">
+          <b>Drinks: </b>We are doing drinks self-serve style. Alcohol is
+          available only to adults of legal drinking age. Please regulate your
+          alcohol consumption. We will have water, non-alcoholic beverages,
+          beer, wine, and cocktails. Beverage contributions are very
+          appreciated.
+        </p>
+        <p className="py-2">
+          <b>Food: </b>We&apos;ll have a variety of options for dinner as well
+          as snacks and desserts. If you have dietary concerns you are welcome
+          to bring your own food. We will have some extra cooler space.
+        </p>
+        <p className="py-2">
+          <b>After hours: </b>Let&apos;s be real, there&apos;s gonna be an after
+          party. Help Kayti and Jake by wrapping up the party promptly at 9 pm
+          so we can get to the next location.
+        </p>
+        <br />
+      </div>
+      <div className="text-center">
+        <p>
+          <AnchorLink href="#info" className="px-1">
+            Click here for FAQs.
+          </AnchorLink>
+        </p>
+        <br />
+        <h3 className="text-lg font-extrabold" id="map">
+          Current Seven Day Forecast
+        </h3>
+        <Suspense fallback={<p>loading...</p>}>
           <a
             className="weatherwidget-io"
             href="https://forecast7.com/en/43d62n116d21/boise/?unit=us"
@@ -33,9 +100,8 @@ const PartyTime = () => {
             data-label_2="WEATHER"
             data-theme="original"
           >
-            BOISE WEATHER
+            Seven Day Boise Weather Forecast
           </a>
-
           {
             !(function (d, s, id) {
               var js,
@@ -48,38 +114,21 @@ const PartyTime = () => {
               }
             })(document, "script", "weatherwidget-io-js")
           }
-
-          <p>What to expect: Lorem ipsum.</p>
-          <p>What to bring: Yourself!</p>
-          <p>Parking: Lorem ipsum.</p>
-          <p>Transportation: Lorem ipsum.</p>
-        </div>
+        </Suspense>
+        <br />
+        <h3 className="text-lg font-extrabold" id="map">
+          Look, a map!
+        </h3>
+        <p>2707 Malibu Road</p>
+        <p>Boise, Idaho 83705</p>
       </div>
-      <div className="w-1/3">
-        <h3 className="text-center text-lg font-extrabold">When</h3>
-        <p>Saturday, August 31, 2024</p>
-        <p>4 pm to 9 pm</p>
-      </div>
-      <div className="w-1/3">
-        <h3 className="text-center text-lg font-extrabold ">Where</h3>
-        <p>Kayti and Jake&apos;s backyard</p>
-      </div>
-      <div className="w-1/3">
-        <h3 className="text-center text-lg font-extrabold ">Details</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, enim
-          laboriosam quasi recusandae nulla quia, debitis incidunt commodi
-          aliquam magni eaque esse blanditiis suscipit assumenda voluptate
-          vitae! Optio, harum at!
-        </p>
-      </div>
-      <NavigationButton linkName="RSVP For the Party" location="/rsvp" />
+      <br />
       <div className="w-screen">
         <Suspense fallback={<p>loading...</p>}>
           <GoogleMap />
         </Suspense>
       </div>
-    </>
+    </div>
   )
 }
 
