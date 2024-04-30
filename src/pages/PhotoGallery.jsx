405e012dbd01bde6728264b08cc902d413fdfb60
Sarah Proctor <sproctor950@gmail.com>
2024-04-30 14:07:43 -0700

    :art: refactors photo gallery asset structure

--- src/pages/PhotoGallery.jsx
+++ src/pages/PhotoGallery.jsx
@@ -1,4 +1,5 @@
 import NavigationBarPage from "../components/NavigationBarPage"
+import PictureModal from "../components/PictureModal"
 import flowerLogo from "../assets/graphics/flower-logo.png"
 import belizeBoat from "../assets/belize-boat.jpeg"
 import bench from "../assets/bench.jpeg"
@@ -22,12 +23,146 @@ import jakeBaby from "../assets/baby-pics/jake-baby.jpg"
 import jakeCasey from "../assets/baby-pics/jake-casey.jpg"
 import jakeRick from "../assets/baby-pics/jake-rick.jpg"
 import jakeSue from "../assets/baby-pics/jake-sue.jpg"
-import kaytiIceCream from "../assets/baby-pics/kayti-icecream.jpg"
+import kaytiIceCream from "../assets/baby-pics/kayti-ice-cream.jpg"
 import kaytiLucy from "../assets/baby-pics/kayti-lucy.jpg"
 import kaytiSarahDad from "../assets/baby-pics/kayti-sarah-dad.jpg"
 import kaytiTub from "../assets/baby-pics/kayti-tub.jpg"
 
 const PhotoGallery = () => {
+  const columnOne = [
+    {
+      image: bolo,
+      alt: "Jake in a bolo tie and Kayti in a green dress",
+      text: ""
+    },
+    {
+      image: goth,
+      alt: "Kayti and Jake in goth Halloween costumes",
+      text: "Goth Halloween"
+    },
+    {
+      image: belizeBoat,
+      alt: "Kayti and Jake on a boat in Belize",
+      text: "Snorkel adventure in Belize"
+    },
+    {
+      image: tableRock,
+      alt: "Kayti and Jake with Lola on the top of Table Rock in Boise",
+      text: "Top of Table Rock with Lola Moon"
+    },
+    { image: riverBend, alt: "Kayti and Jake in front of a winding river" },
+    {
+      image: punch,
+      alt: "Kayti and Jake sharing a large cup of red drink with two crazy straws",
+      text: ""
+    },
+    {
+      image: boiseRiver,
+      alt: "Jake and Kayti wearing sunglasses floating the river",
+      text: "Floating the Boise River"
+    },
+    {
+      image: fancy,
+      alt: "Kayti and Jake all dressed up for an event",
+      text: ""
+    },
+    {
+      image: christmas,
+      alt: "Kayti and Jake in front of a lit Christmas tree",
+      text: "Happy holidays!"
+    },
+    {
+      image: river,
+      alt: "Kayti and Jake camping in front of a river",
+      text: ""
+    }
+  ]
+  const columnTwo = [
+    {
+      image: downtown,
+      alt: "Kayti and Jake kissing under a structure at Treefort",
+      text: ""
+    },
+    {
+      image: engagement,
+      alt: "Kayti and Jake kissing on the top of Xunantunich monument in Belize",
+      text: "Newly engaged on the Xunantunich monument in Belize."
+    },
+    {
+      image: lights,
+      alt: "Kayti and Jake in front of a web of Christmas lights",
+      text: ""
+    },
+    {
+      image: lolaBaby,
+      alt: "Kayti and Jake with new pup Lola Moon",
+      text: "Lola Moon"
+    },
+    {
+      image: mccall,
+      alt: "Kayti and Jake on the shore of a lake",
+      text: "Kayti and Jake in McCall"
+    },
+    {
+      image: lolaCar,
+      alt: "Kayti and Jake and Lola in the car",
+      text: ""
+    },
+    {
+      image: bench,
+      alt: "Kayti and Jake on David's bench on the Boise greenbelt",
+      text: "Kayti and Jake on David's memorial bench."
+    },
+    {
+      image: hikingLola,
+      alt: "Kayti and Jake with Lola in the foothills",
+      text: ""
+    }
+  ]
+  const jakeBabyPics = [
+    {
+      image: jakeBaby,
+      alt: "Baby Jake all wrapped in blankets",
+      text: "Baby Jake"
+    },
+    {
+      image: jakeCasey,
+      alt: "Kiddo Jake and Casey in fancy dress clothes",
+      text: "Jake and sister, Casey"
+    },
+    {
+      image: jakeRick,
+      alt: "Jake and Papa Rick in cowboy boots and hat",
+      text: "Jake and Papa Rick"
+    },
+    {
+      image: jakeSue,
+      alt: "Jake and Momma Sue",
+      text: "Jake and Momma Sue"
+    }
+  ]
+  const kaytiBabyPics = [
+    {
+      image: kaytiTub,
+      alt: "Baby Kayti in a bathtub of bubbles",
+      text: "Bubble Kayti"
+    },
+    {
+      image: kaytiLucy,
+      alt: "Kayti sledding with Lucy Dog",
+      text: "Kayti and Lucy Dog"
+    },
+    {
+      image: kaytiIceCream,
+      alt: "Kiddo Kayti eating ice cream wearing a sun hat",
+      text: "Kiddo Kayti"
+    },
+    {
+      image: kaytiSarahDad,
+      alt: "Kayti and sister, Sarah, on Dad's back",
+      text: "Kayti with sister, Sarah, playing with Dad"
+    }
+  ]
   return (
     <>
       <NavigationBarPage />
@@ -36,148 +171,24 @@ const PhotoGallery = () => {
       </h2>
       <div className="flex justify-center">
         <div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={bolo}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={goth}
-              alt="Kayti and Jake in goth Halloween costumes"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">Goth Halloween</p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={belizeBoat}
-              alt="Kayti and Jake on a boat in Belize"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">Snorkel adventure in Belize</p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={tableRock}
-              alt="Kayti and Jake with Lola on the top of Table Rock in Boise"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">
-              Top of Table Rock with Lola Moon
-            </p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={riverBend}
-              alt="Kayti and Jake in front of a winding river"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={punch}
-              alt="Kayti and Jake sharing a large cup of red drink with two crazy straws"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={boiseRiver}
-              alt="Jake and Kayti wearing sunglasses floating the river"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">Floating the Boise River</p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={fancy}
-              alt="Kayti and Jake all dressed up for an event"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={christmas}
-              alt="Kayti and Jake in front of a lit Christmas tree"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">Happy holidays!</p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={river}
-              alt="Kayti and Jake camping in front of a river"
-              className="h-auto w-max"
-            />
-          </div>
+          {columnOne.map((picture, index) => {
+            return (
+              <div className="w-40 px-2 pb-2 text-center md:w-80" key={index}>
+                <PictureModal source={picture.image} altNext={picture.alt} />
+                <p className="text-xs text-gray">{picture.text}</p>
+              </div>
+            )
+          })}
         </div>
         <div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={downtown}
-              alt="Kayti and Jake kissing under a structure at Treefort"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={lolaBaby}
-              alt="Kayti and Jake with new pup Lola Moon"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">Lola Moon</p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={mccall}
-              alt="Kayti and Jake on the shore of a lake"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={lolaCar}
-              alt="Kayti and Jake and Lola in the car"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={engagement}
-              alt="Kayti and Jake kissing on the top of Xunantunich monument in Belize"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">
-              Newly engaged on the Xunantunich monument in Belize.
-            </p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={bench}
-              alt="Kayti and Jake on David's bench on the Boise greenbelt"
-              className="h-auto w-max"
-            />
-            <p className="text-xs text-gray">
-              Kayti and Jake on David&apos;s memorial bench.
-            </p>
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={hikingLola}
-              alt="Kayti and Jake with Lola in the foothills"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={lights}
-              alt="Kayti and Jake in front of a web of Christmas lights"
-              className="h-auto w-max"
-            />
-          </div>
+          {columnTwo.map((picture, index) => {
+            return (
+              <div className="w-40 px-2 pb-2 text-center md:w-80" key={index}>
+                <PictureModal source={picture.image} altNext={picture.alt} />
+                <p className="text-xs text-gray">{picture.text}</p>
+              </div>
+            )
+          })}
         </div>
       </div>
       <p className="m-2 text-center font-alice text-xl uppercase text-gray md:text-2xl">
@@ -185,64 +196,24 @@ const PhotoGallery = () => {
       </p>
       <div className="flex justify-center">
         <div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={jakeBaby}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={jakeCasey}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={jakeRick}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={jakeSue}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
+          {jakeBabyPics.map((picture, index) => {
+            return (
+              <div className="w-40 px-2 pb-2 text-center md:w-80" key={index}>
+                <PictureModal source={picture.image} altNext={picture.alt} />
+                <p className="text-xs text-gray">{picture.text}</p>
+              </div>
+            )
+          })}
         </div>
         <div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={kaytiTub}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={kaytiLucy}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={kaytiIceCream}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
-          <div className="w-40 px-2 pb-2 text-center md:w-80">
-            <img
-              src={kaytiSarahDad}
-              alt="Jake in a bolo tie and Kayti in a green dress"
-              className="h-auto w-max"
-            />
-          </div>
+          {kaytiBabyPics.map((picture, index) => {
+            return (
+              <div className="w-40 px-2 pb-2 text-center md:w-80" key={index}>
+                <PictureModal source={picture.image} altNext={picture.alt} />
+                <p className="text-xs text-gray">{picture.text}</p>
+              </div>
+            )
+          })}
         </div>
       </div>
       <div className="flex justify-center">
