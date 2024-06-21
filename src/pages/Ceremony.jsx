2f08968a468e2f06948d66b97819c34f2de429aa
sjproctor <sproctor950@gmail.com>
2024-06-21 13:17:29 -0700

    :sparkles: adds information and images from the ceremony

--- src/pages/Ceremony.jsx
+++ src/pages/Ceremony.jsx
@@ -2,6 +2,10 @@ import NavigationBarPage from "../components/NavigationBarPage"
 import marryMe from "../assets/marry-me.jpeg"
 import license from "../assets/license.jpeg"
 import treeLogo from "../assets/graphics/tree-logo.png"
+import wedding from "../assets/ceremony/wedding.png"
+import cakeChampagne from "../assets/ceremony/cake-champagne.jpg"
+import jakeErikKayti from "../assets/ceremony/jake-erik-kayti.jpg"
+import justMarried from "../assets/ceremony/just-married.jpg"
 
 const Ceremony = () => {
   return (
@@ -20,10 +24,37 @@ const Ceremony = () => {
       <div className="m-auto w-11/12 text-center md:w-1/2">
         <h3 className="text-lg font-extrabold">The Wedding</h3>
         <p>
-          Kayti and Jake will be tying the knot in a small ceremony in early
-          June.
+          Kayti and Jake tied the knot in a small ceremony on June 8th, 2024, on
+          the banks of the Middle Fork Payette River in Garden Valley. They were
+          joined by their immediate family and closest friends.
         </p>
         <br />
+        <div className="flex justify-center">
+          <img
+            src={wedding}
+            alt="Kayti and Jake getting married with officiant Erik"
+            className="mb-8 h-auto w-72"
+          />
+        </div>
+        <p>
+          The bride and groom had a wonderful weekend and are thankful to their
+          family and friends for creating exactly the wedding they envisioned.
+        </p>
+        <br />
+        <div className="flex justify-center">
+          <img
+            src={cakeChampagne}
+            alt="Cake, cupcakes, and champagne flute display adorned with wildflowers and taper candles"
+            className="mb-8 h-auto w-72"
+          />
+        </div>
+        <div className="flex justify-center">
+          <img
+            src={justMarried}
+            alt="Kayti and Jake leaning on a pop up trailer decorated with a sign reading 'Just Married' and painted tin cans strung from the bumper"
+            className="mb-8 h-auto w-72"
+          />
+        </div>
         <h3 className="text-lg font-extrabold">Officiant Erik</h3>
         <p>
           Kayti and Jake asked long time friend and partner-in-law, Erik
@@ -38,9 +69,13 @@ const Ceremony = () => {
             className="mb-8 h-auto w-72"
           />
         </div>
-        <h3 className="text-lg font-extrabold">Updates Coming!</h3>
-        <p>Check back in mid-June for details and photos from the ceremony.</p>
-        <br />
+        <div className="flex justify-center">
+          <img
+            src={jakeErikKayti}
+            alt="Selfie of Jake, Erik, and Kayti in front of the river"
+            className="mb-8 h-auto w-72"
+          />
+        </div>
       </div>
       <div className="flex justify-center">
         <img
