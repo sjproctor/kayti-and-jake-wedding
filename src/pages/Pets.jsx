61c6e891d42d8379495947a9007e1828f46acd7b
sjproctor <sproctor950@gmail.com>
2024-06-21 13:18:01 -0700

    :sparkles: adds more pet images because pets are adorable

--- src/pages/Pets.jsx
+++ src/pages/Pets.jsx
@@ -3,6 +3,11 @@ import NavigationBarPage from "../components/NavigationBarPage"
 import treeLogo from "../assets/graphics/tree-logo.png"
 import lolaWrap from "../assets/pets/lola-wrap.png"
 import toastDishwasher from "../assets/pets/toast-dishwasher.jpeg"
+import kaytiBaby from "../assets/pets/kayti-baby.jpeg"
+import sleepingLola from "../assets/pets/sleeping-lola.jpeg"
+import tuxBurrito from "../assets/pets/tux-burrito.jpg"
+import lolaPatio from "../assets/pets/lola-patio.jpg"
+import babyNecktie from "../assets/pets/baby-necktie.jpg"
 
 const Pets = () => {
   return (
@@ -28,17 +33,77 @@ const Pets = () => {
           .
         </p>
       </div>
-      <div className="mb-4 flex justify-center">
+
+      <div className="mb-4 flex flex-wrap justify-center">
         <div>
-          <img
-            src={toastDishwasher}
-            alt="Toast kitty in the dishwasher"
-            className="h-auto w-64"
-            loading="lazy"
-          />
-          <p className="w-64 text-center text-xs text-gray">
-            Toast kitty in the dishwasher.
-          </p>
+          <div>
+            <img
+              src={babyNecktie}
+              alt="Baby Dog sleeping on a wooden deck wearing a necktie that says 'I Do Too'"
+              className="mx-2 h-auto w-64"
+              loading="lazy"
+            />
+            <p className="mb-2 w-64 text-center text-xs text-gray">
+              Baby Dog in her wedding attire.
+            </p>
+          </div>
+          <div>
+            <img
+              src={toastDishwasher}
+              alt="Toast kitty in the dishwasher"
+              className="mx-2 h-auto w-64"
+              loading="lazy"
+            />
+            <p className="mb-2 w-64 text-center text-xs text-gray">
+              Toast kitty in the dishwasher.
+            </p>
+          </div>
+          <div>
+            <img
+              src={kaytiBaby}
+              alt="Kayti and Baby Dog chilling on a trampoline"
+              className="mx-2 h-auto w-64"
+              loading="lazy"
+            />
+            <p className="mb-2 w-64 text-center text-xs text-gray">
+              Kayti and Baby Dog.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img
+              src={sleepingLola}
+              alt="Lola dog sleeping on a couch piled with pillows"
+              className="mx-2 h-auto w-64"
+              loading="lazy"
+            />
+            <p className="mb-2 w-64 text-center text-xs text-gray">
+              Sleeping Lola dog.
+            </p>
+          </div>
+          <div>
+            <img
+              src={tuxBurrito}
+              alt="Tux wrapped in a blanket burrito"
+              className="mx-2 h-auto w-64"
+              loading="lazy"
+            />
+            <p className="mb-2 w-64 text-center text-xs text-gray">
+              Tux burrito.
+            </p>
+          </div>
+          <div>
+            <img
+              src={lolaPatio}
+              alt="Lola on the cabin patio"
+              className="mx-2 h-auto w-64"
+              loading="lazy"
+            />
+            <p className="mb-2 w-64 text-center text-xs text-gray">
+              Lola at the wedding.
+            </p>
+          </div>
         </div>
       </div>
 
