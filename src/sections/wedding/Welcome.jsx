4ca0bdb532f33eebc6f59aa87ac8abb713e7300c
sjproctor <sproctor950@gmail.com>
2025-06-23 22:25:57 -0700

    :recycle: removes duplicate styles

--- src/sections/wedding/Welcome.jsx
+++ src/sections/wedding/Welcome.jsx
@@ -1,11 +1,11 @@
 const Welcome = () => {
   return (
-    <div className="mx-4 mt-4 md:mx-40 md:mt-16">
-      <p className="text-center font-alice text-2xl uppercase text-gray md:text-4xl">
+    <div className="mx-4 mt-4 text-center md:mx-40 md:mt-16">
+      <p className="font-alice text-2xl uppercase text-gray md:text-4xl">
         Kayti and Jake got married!
       </p>
       <br />
-      <div className=" bg-champagne px-2 py-4 text-center md:p-8 md:text-lg">
+      <div className=" bg-champagne px-2 py-4 md:p-8 md:text-lg">
         <p className="p-1">Hello and welcome. You made it!</p>
         <p className="p-1">
           If you are reading this that means you are a very special part of
